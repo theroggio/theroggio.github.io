fe811468b856c0346b87fe2cf76ff2498d21176f
Gianmarco <groggiol@uni-bonn.de>
2024-08-29 16:11:04 -0700

    try oktokit

--- js/writing.js
+++ js/writing.js
@@ -2,9 +2,17 @@ window.addEventListener("load", async()=>{
    const template = document.querySelector("template").content.querySelector(".li");
    const ul = document.querySelector("#ul");
    
+   const thing = await octokit.request('GET /repos/theroggio/theroggio.github.io/contents/writing', {
+      owner: 'theroggio',
+      repo: 'theroggio.github.io',
+      path: 'writing',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+   });
+   console.log(thing);
    const response = await fetch(
-      "https://api.github.com/repos/theroggio/theroggio.github.io/git/tree/main/writing?recursive=1"
-   );
+      "https://api.github.com/repos/theroggio/theroggio.github.io/git/tree/main/writing");
    if(response.ok){
        const files = await response.json();
        const html = files.tree.filter(f=>/.html$/.test(f.path));
