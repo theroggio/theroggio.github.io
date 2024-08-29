b501ddeebc55c6179984b2a5cfaf36d2e721ef59
Gianmarco <groggiol@uni-bonn.de>
2024-08-29 15:43:56 -0700

    trial 3/?

--- js/writing.js
+++ js/writing.js
@@ -1,18 +1,28 @@
-import fs from 'fs';
-
-var files = fs.readdirSync(dir);
-var filelist = [];
-files.forEach(function(file) {
-   filelist.push(file);
+window.addEventListener("load", async()=>{
+   const template = document.querySelector("template").content.querySelector(".li");
+   const ul = document.querySelector("#ul");
+   
+   const response = await fetch(
+       "https://github.com/theroggio/theroggio.github.io/tree/main/writing"
+   );
+   if(response.ok){
+       const files = await response.json();
+       const html = files.tree.filter(f=>/.html$/.test(f.path));
+       html.forEach(f=>createList(f))
+   }
+   
+   function createList(file){
+       const li = template.cloneNode(true);
+       li.addEventListener("click",e=>{
+           li.classList.toggle("active");
+       });
+       li.querySelector("span").innerText = file.path;
+       fetch(file.url).then(r=>{
+           if(!r.ok) return;
+           r.json().then(t=>li.querySelector("div").innerText = atob(t.content))
+       });
+       ul.appendChild(li);
+       console.log(li);
+   }
+   
    });
-console.log(filelist[0]);
-fs.readFile(filelist[0], 'utf8', (err, data) )
-console.log(data);
-console.log(err);
-const para = document.createElement("p");
-const node = document.createTextNode(data);
-para.appendChild(node);
-
-const element = document.getElementById("footer");
-const child = document.getElementById("p");
-element.insertBefore(para, child);
