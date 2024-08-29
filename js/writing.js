e4691206ad700abdd20ae6982ac9db37773031bd
Gianmarco <groggiol@uni-bonn.de>
2024-08-29 15:18:27 -0700

    debug js file, part 1/?

--- js/writing.js
+++ js/writing.js
@@ -1,11 +1,11 @@
-import * as fs from 'fs';
-var files = fs.readdirSync(dir);
+import { readdirSync, readFile } from 'node:fs';
+var files = readdirSync(dir);
 var filelist = [];
 files.forEach(function(file) {
    filelist.push(file);
    });
 console.log(filelist[0]);
-fs.readFile('./content/generic.md', 'utf8', (err, data) )
+readFile('./content/generic.md', 'utf8', (err, data) )
 console.log(data);
 console.log(err);
 const para = document.createElement("p");
