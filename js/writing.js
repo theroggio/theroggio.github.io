fb0e9151495889519b47086ae0c9acd640ee33f5
Gianmarco <groggiol@uni-bonn.de>
2024-08-29 15:22:30 -0700

    try different import 2/?

--- js/writing.js
+++ js/writing.js
@@ -1,11 +1,12 @@
-import { readdirSync, readFile } from 'node:fs';
-var files = readdirSync(dir);
+import fs from 'fs';
+
+var files = fs.readdirSync(dir);
 var filelist = [];
 files.forEach(function(file) {
    filelist.push(file);
    });
 console.log(filelist[0]);
-readFile('./content/generic.md', 'utf8', (err, data) )
+fs.readFile(filelist[0], 'utf8', (err, data) )
 console.log(data);
 console.log(err);
 const para = document.createElement("p");
