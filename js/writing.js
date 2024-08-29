7fc4edcf61a3da0fe887a4e8b5b8c8b4ff1e1feb
Gianmarco <groggiol@uni-bonn.de>
2024-08-29 15:14:13 -0700

    trying with external js

---
+++ js/writing.js
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+var files = fs.readdirSync(dir);
+var filelist = [];
+files.forEach(function(file) {
+   filelist.push(file);
+   });
+console.log(filelist[0]);
+fs.readFile('./content/generic.md', 'utf8', (err, data) )
+console.log(data);
+console.log(err);
+const para = document.createElement("p");
+const node = document.createTextNode(data);
+para.appendChild(node);
+
+const element = document.getElementById("footer");
+const child = document.getElementById("p");
+element.insertBefore(para, child);
