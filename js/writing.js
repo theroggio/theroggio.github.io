a66161b91e260ef8c2266ba2758239464f34a8aa
Gianmarco <groggiol@uni-bonn.de>
2024-09-04 16:49:53 -0700

    fixed writing.html nice

--- js/writing.js
+++ js/writing.js
@@ -3,20 +3,22 @@ window.addEventListener("load", async()=>{
    const ul = document.querySelector("#ul");
    
    const urls = [ 
-    "https://api.github.com/repos/theroggio/theroggio.github.io/contents/writing/nantra_ancora.html",
-    "https://api.github.com/repos/theroggio/theroggio.github.io/contents/writing/il_folle.html",
-    "https://api.github.com/repos/theroggio/theroggio.github.io/contents/writing/infrangente.html",
-    "https://api.github.com/repos/theroggio/theroggio.github.io/contents/writing/lit.html",
-    "https://api.github.com/repos/theroggio/theroggio.github.io/contents/writing/mancanze_in_quarantena.html",
+    "https://theroggio.github.io/writing/nantra_ancora.html",
+    "https://theroggio.github.io/writing/il_folle.html",
+    "https://theroggio.github.io/writing/infrangente.html",
+    "https://theroggio.github.io/writing/lit.html",
+    "https://theroggio.github.io/writing/mancanze_in_quarantena.html",
    ];
 
    for (let i = 0; i < urls.length; i++){
-    const li = template.cloneNode(true);
-    fetch(urls[i]).then(async r=>{
+      const li = template.cloneNode(true);
+      const li_fake = template.cloneNode(true);
+      fetch(urls[i]).then(async r=>{
        const thing = await r.text();
        li.querySelector("div").innerHTML = thing
     });
     ul.appendChild(li);
+    ul.appendChild(li_fake);
    }
    
    });
